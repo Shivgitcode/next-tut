e5aa7e453f3026793282b78018db6932ee1c63e2
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-02-21 19:54:12 +0530

    done private routes and route groups

---
+++ routing-demo/src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+
+export default function Register() {
+    return (
+        <div>register
+            <Hero></Hero>
+
+        </div>
+    )
+}
---
+++ routing-demo/src/components/Hero.tsx
@@ -0,0 +1,6 @@
+
+export default function Hero() {
+    return (
+        <div>this is hero section</div>
+    )
+}
