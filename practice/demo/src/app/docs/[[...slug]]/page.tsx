851ec7c00c4b1f25d133f563d1dd520490f9dbd8
shivansh <shivneeraj2004@gmail.com>
2024-03-14 13:06:23 +0530

    link navigation

--- practice/demo/src/app/docs/[[...slug]]/page.tsx
+++ practice/demo/src/app/docs/[[...slug]]/page.tsx
@@ -1,18 +1,12 @@
+export const metadata = {
+  title: "Docs",
+};
 
 export default function Docs({ params }: { params: { slug: string[] } }) {
-
-    if (params.slug?.length === 2) {
-        return (
-            <div>{params.slug[1]}</div>
-        )
-    }
-    else if (params.slug?.length <= 2) {
-        return (
-            <div>{params.slug[0]}</div>
-        )
-    }
-    return (
-        <div>docs page</div>
-    )
-
+  if (params.slug?.length === 2) {
+    return <div>{params.slug[1]}</div>;
+  } else if (params.slug?.length <= 2) {
+    return <div>{params.slug[0]}</div>;
+  }
+  return <div>docs page</div>;
 }
--- practice/demo/src/app/page.tsx
+++ practice/demo/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <h1>
+      Home
+      <Link href="/docs">Docs page</Link>
+      <Link href="/docs/feature1">features</Link>
+    </h1>
+  );
 }
